ed565aa3ecb1dd2376ec1f46f901a4403ed37d13
BeneArkoh <101414576+BeneArkoh@users.noreply.github.com>
2022-04-04 15:03:16 -0700

    react-component-8

---
+++ src/Pages/Blog.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section>
+      <h1>Welcome to our Blog Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        doloribus dicta vel illo omnis deleniti consequatur ratione numquam!
+        Quis labore repellat, tenetur, provident nihil laborum autem tempore
+        porro ipsum consectetur ipsa error, quibusdam adipisci magni.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        doloribus dicta vel illo omnis deleniti consequatur ratione numquam!
+        Quis labore repellat, tenetur, provident nihil laborum autem tempore
+        porro ipsum consectetur ipsa error, quibusdam adipisci magni.
+      </p>
+    </section>
+  );
+};
+
+export default Blog;
---
+++ src/Pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Welcome to our Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        doloribus dicta vel illo omnis deleniti consequatur ratione numquam!
+        Quis labore repellat, tenetur, provident nihil laborum autem tempore
+        porro ipsum consectetur ipsa error, quibusdam adipisci magni.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        doloribus dicta vel illo omnis deleniti consequatur ratione numquam!
+        Quis labore repellat, tenetur, provident nihil laborum autem tempore
+        porro ipsum consectetur ipsa error, quibusdam adipisci magni.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
